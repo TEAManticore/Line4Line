4a23821c28437120b5357a6556827171df607a33
Darion Freeman <darionfman@gmail.com>
2016-10-13 10:55:39 -0500

    Removed unecesary collections

--- server/collections/stories.js
+++
@@ -1,7 +0,0 @@
-var Story = require('../models/story');
-
-var Stories = new db.Collection();
-
-Stories.model = Story;
-
-module.exports = Stories;
--- server/collections/users.js
+++
@@ -1,7 +0,0 @@
-var User = require('../models/story');
-
-var Users = new db.Collection();
-
-Users.model = Story;
-
-module.exports = Users;
