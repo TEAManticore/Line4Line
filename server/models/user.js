bf274a12c121633923d36ac348ed40d77db2493f
Darion Freeman <darionfman@gmail.com>
2016-10-13 09:58:11 -0500

    Model for a User document, includes mongoose and bcrypt

---
+++ server/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const Schema = mongoose.Schema
+
+const listSchema = require('./List.schema.js')
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  sessions: [ String ],
+  stories: [ String ] 
+})
+
+userSchema.pre('save', function (next) {
+  const user = this
+  // if the user never updated their password
+  // then no need to has password
+  if (!user.isModified('password')) {
+    next()
+  } else {
+    bcrypt.hash(user.password, saltRounds, function (err, hash) {
+      if (err) next(err)
+      user.password = hash
+      next()
+    })
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
