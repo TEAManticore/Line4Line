bec12842cfcc731c57535f6273a5d58388123680
Darion Freeman <darionfman@gmail.com>
2016-10-13 09:37:25 -0500

    creates basic server default port 8080

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const cookie = require('cookie-parser')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 8080
+// app level middleware
+app.use(express.static(path.resolve(__dirname, '../client/public')))
+app.use(morgan('dev'))
+app.use(cookie())
+
+app.get('/', (req,res) =>{
+  res.render('index.html')
+})
+
+const server = app.listen(port)
+console.log(`Server is running on port: ${port}`)
+
+// export server for testing
+module.exports = {
+  server,
+  app
+}
