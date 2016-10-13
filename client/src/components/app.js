1d2e5d1269eb0f83820b8053acf40a61492d0b5c
Phil <plmok61@gmail.com>
2016-10-13 16:43:59 -0500

    lobby renders in the App.js

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import Lobby from './Lobby'
 
 class App extends React.Component {
   constructor (props) {
     super(props)
+    this.state = {
+      currentView: 'Login'
+    }
   }
 
   render () {
     return (
-      <div> HELLO THERE </div>
+      <Lobby />
     )
   }
 }
---
+++ client/src/components/lobby.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Lobby extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+  
+  render () {
+    return (
+      <div>
+        <h3>Join a story or create your own</h3>
+      </div>
+    )
+  }
+}
+
+export default Lobby
