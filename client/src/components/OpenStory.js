462ae2c53be1149d0dc32f92deb9a1a7b7d0f8ed
Phil <plmok61@gmail.com>
2016-10-15 15:32:32 -0500

    added some comments for when we are able to make post requests

--- client/src/components/OpenStory.js
+++ client/src/components/OpenStory.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import Request from 'request'
 
 const OpenStory = (props) => {
+
+  // joinStory () {
+  //  First we will need to get the current user's info to add him/her to the story in the DB
+  //
+  //   Request.post(`/stories/${props.story.id}`).on('response', function(response) {
+  //     console.log('Got stories: ', response.statusCode)
+  //   })
+  // }
+
   return (
     <div className="openStoryWrap">
       <div className="openStoryTitle">{props.story.title}</div>
