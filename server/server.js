9e99edbe75cc614abab61615fc08aa59d10d65a7
Darion Freeman <darionfman@gmail.com>
2016-10-20 14:13:41 -0500

    Finally fixes the minor auth error in routes middleware

--- server/server.js
+++ server/server.js
@@ -1,30 +1,30 @@
 const express          = require('express')
 const bodyParser       = require('body-parser')
+const app              = express()
 const passport         = require('passport')
 const FacebookStrategy = require('passport-facebook').Strategy
 const session          = require('express-session')
 const path             = require('path')
 const morgan           = require('morgan')
-const app              = express()
 const router           = require('./routes/routes')
 const User             = require('./models/user')
 
 const port             = process.env.PORT || 8081
 
 passport.serializeUser(function (user, done) {
+  console.log(user)
   done(null, user)
 })
 
 passport.deserializeUser(function (obj, done) {
-  done(null, obj)
+  done(null,obj)
 })
 
 passport.use(new FacebookStrategy({
     clientID          : process.env.CLIENTID,
     clientSecret      : process.env.CLIENTSECRET,
     callbackURL       : "http://localhost:8081/auth/facebook/return",
-    passReqToCallback : true,
-    profileFields     : ['id', 'displayName'],
+    passReqToCallback : true
   },
   function(req, token, refreshToken, profile, done) {  
     console.log('refreshToken:',refreshToken)
@@ -55,17 +55,8 @@ passport.use(new FacebookStrategy({
     throw err
   }) 
 }))
-
-// app level middleware
-app.use(morgan('dev'))
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  //res.setHeader('Content-Type', 'application/JSON')
-  // res.header('Access-Control-Allow-Credentials', true)
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, '../dist')))
 app.use(session({ 
   secret: process.env.SECRET, 
   resave: true,
@@ -73,15 +64,15 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.resolve(__dirname, '../dist')))
-
+app.use(morgan('dev'))
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  //res.setHeader('Content-Type', 'application/JSON')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 app.use('/', router)
-
-const server = app.listen(port)
 console.log(`Server is running on port: ${port}`)
+app.listen(port)
 
-// export server for testing
-module.exports = {
-  server,
-  app
-}
