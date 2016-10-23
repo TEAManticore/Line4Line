e6276a77b2b18d16315272e482230d0df1bebd55
Phil <plmok61@gmail.com>
2016-10-22 20:23:16 -0500

    this code is not DRY but it works - still need to test with mulitple users

--- client/src/components/story.js
+++ client/src/components/story.js
@@ -18,6 +18,7 @@ class Story extends React.Component {
       currentLine: 0,
       lines: [],
       currentUser: null,
+      currentUserIndex: 0,
       prevLineIndex: 0
     }
   }
@@ -39,14 +40,16 @@ class Story extends React.Component {
         currentLine: story.currentLine,
         lines: story.lines
       })
+      console.log('lines',this.state.lines)
       $.get('http://localhost:8081/user')
-      .then(user => {
+      .then(user => {  
         
-        
-        const prevLineIndex = (this.state.currentLine ? this.state.currentLine - 1 : this.state.currentLine)
+        const currentUserIndex = this.state.users.indexOf(user.id)
+        const prevLineIndex = (currentUserIndex ? currentUserIndex - 1 : currentUserIndex)
 
         this.setState({
           currentUser: user,
+          currentUserIndex: currentUserIndex,
           prevLineIndex: prevLineIndex
         })
         console.log('currentUser: ', this.state.currentUser)
@@ -58,30 +61,73 @@ class Story extends React.Component {
 
       
   render(){
+
     if (this.state.currentUser) {
+      
       const prevLine = this.state.lines[this.state.prevLineIndex]
       const currLine = {userId: this.state.currentUser.id, text: '', story: this.state.storyId}
-      return (
-        <div className="storyContainer" >  
-          <h2 className="title">{ this.state.title }</h2>
-          
-          {
-            this.state.prevLineIndex === 0 
-
-            ?
-
-            <Line line={currLine} lock={false} />
+      const currComplete = this.state.lines[this.state.currentUserIndex]
+     
+      if (this.state.currentLine === 0 && this.state.currentUserIndex === 0) {
+      //If the current user is the creator of the story and has not written a line yet  
+        return (
+          <div className="storyContainer" >  
+            <h2 className="title">{ this.state.title }</h2>
             
-            :
+            <Line line={currLine} lock={false} />
 
+          </div>  
+        )
+        
+      } else if (this.state.currentLine > 0 && this.state.currentUserIndex === 0) {
+      //If the current user is the creator of the story and has already written their line 
+        return (
+          <div className="storyContainer" >  
+            <h2 className="title">{ this.state.title }</h2>
+            
+            <Line line={currComplete} lock={true} />
+
+          </div>  
+        )
+
+      } else if (this.state.currentLine > this.state.currentUserIndex) {
+      //If the current user is not the creator and has already written their line
+        return (
+          <div className="storyContainer" >  
+            <h2 className="title">{ this.state.title }</h2>
+            
+            <div>
+              <Line line={prevLine} lock={true} />
+              <Line line={currComplete} lock={true} />
+            </div>
+
+          </div>  
+        )
+      } else if (this.state.currentLine !== this.state.currentUserIndex) {
+      //If the current user is not the creator and has not written their line and it is not their turn
+        return (
+          <div className="storyContainer" >  
+            <h2 className="title">{ this.state.title }</h2>
+            
+            <h3>Not your turn!</h3>
+
+          </div>  
+        )
+      } else {
+      //If the current user is not the creator and has not written their line and it is their turn to write
+         return (
+          <div className="storyContainer" >  
+            <h2 className="title">{ this.state.title }</h2>
+            
             <div>
               <Line line={prevLine} lock={true} />
               <Line line={currLine} lock={false} />
             </div>
-          }
 
-        </div>  
-      )
+          </div>  
+        )
+        
+      }
     } else {
       return (
         <div>
