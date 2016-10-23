bbabb2dbe34d4895ed8bf9d52776f5ff185f2913
Phil <plmok61@gmail.com>
2016-10-23 15:45:13 -0500

    show complete story

--- client/src/components/lobby.js
+++ client/src/components/lobby.js
@@ -12,6 +12,7 @@ class Lobby extends React.Component {
   }
 
   componentDidMount () {
+    //get an array of all the stories from the db that need more users
     $.get('http://localhost:8081/stories')
     .then(stories => {
       console.log('Got stories: ', stories);
