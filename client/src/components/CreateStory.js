0deb363e453cd92d87af30629d9e4af56f0a373d
Darion Freeman <darionfman@gmail.com>
2016-10-23 12:16:23 -0500

    Fixes the create story to only relate to the number of users

--- client/src/components/CreateStory.js
+++ client/src/components/CreateStory.js
@@ -7,7 +7,6 @@ const CreateStory = (props) => {
     const newStory = {}
     newStory.title = document.getElementById('createTitle').value
     newStory.numberUsers = document.getElementById('createNUsers').value
-    newStory.storyLength = document.getElementById('createLength').value
     $.ajax({
       type: 'POST',
       url:'http://localhost:8081/stories', 
@@ -30,10 +29,6 @@ const CreateStory = (props) => {
         <div>
           <h3>Number of users</h3>
           <input className="createStoryInput createNumberInput" id="createNUsers" type="number" min="1" max="10" placeholder="5"/>
-        </div>
-        <div>  
-          <h3>Story Length</h3>
-          <input className="createStoryInput createNumberInput" id="createLength" type="number" min="5" max="20" placeholder="10"/>
         </div>
         <div className='createButtonWrap'>
           <input className="standardButton blackButton" type="submit" value="Create" />
